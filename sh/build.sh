a576bc737b42f691ad4020d54942c2e79036948b
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-12-17 16:23:44 +0000

    Applied some bug fixes.

--- sh/build.sh
+++ sh/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+shx generate
 ls -1 build/*.zip | while IFS= read -r file; do
-	rm -v "build/${file}.zip"
+	rm -v "${file}"
 done
 cd src
 ls -1 . | while IFS= read -r folder; do
 	cd "${folder}"
 	echo "Generating \"${folder}.zip\"..."
-	zip -r9 "../../build/${folder}.zip" * .*
+	zip -r9q "../../build/${folder}.zip" * .*
 	cd ..
 done
 exit
