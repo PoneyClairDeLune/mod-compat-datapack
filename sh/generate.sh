05febcef4b0d98501ea0cf1d3e83889debac214f
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-01-03 18:06:14 +0000

    Merged TNT recipes and refined sand tags.

--- sh/generate.sh
+++ sh/generate.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+echo "Removing previously-generated tags..."
+tree -ifl | grep -E "src/.*/data/.*/tags/(items|block)/" | grep -E "\.json$" | while IFS= read -r file; do
+	rm -v "${file}"
+done
 if [ -e conf/item_tags ]; then
-	ls -1 conf/item_tags | while IFS= read -r file; do
+	ls -1 conf/item_tags | grep -E "\.tsv$" | while IFS= read -r file; do
 		echo "Generating item tags for \"${file}\"..."
 		deno run --allow-read --allow-write conf/datagen/tags.js items ${file}
 	done
 fi
 if [ -e conf/block_tags ]; then
-	ls -1 conf/block_tags | while IFS= read -r file; do
+	ls -1 conf/block_tags | grep -E "\.tsv$" | while IFS= read -r file; do
 		echo "Generating block tags for \"${file}\"..."
 		deno run --allow-read --allow-write conf/datagen/tags.js block ${file}
 	done
 fi
 if [ -e conf/ptr_tags ]; then
-	ls -1 conf/ptr_tags | while IFS= read -r file; do
+	ls -1 conf/ptr_tags | grep -E "\.tsv$" | while IFS= read -r file; do
 		echo "Generating pointer tags for \"${file}\"..."
 		deno run --allow-read --allow-write conf/datagen/tags_ptr.js ${file}
 	done
