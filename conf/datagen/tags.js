bffea769ced0974375deb9fc1d96aa70395b6ec6
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-01-10 16:20:35 +0000

    Datagen now fills the missing folders.

--- conf/datagen/tags.js
+++ conf/datagen/tags.js
@@ -14,9 +14,31 @@ if (!dataReadDir) {
 	console.error(`Unknown datapack type: ${datapackType}`);
 	Deno.exit(1);
 };
+self.Deno.hasFileSync = (path) => {
+	try {
+		Deno.statSync(path);
+		return true;
+	} catch (err) {
+		return false;
+	};
+};
+self.Deno.hasFile = async (path) => {
+	try {
+		await Deno.stat(path);
+		return true;
+	} catch (err) {
+		return false;
+	};
+};
 let getNamespacedPath = (datapackId, namespacedId, type) => {
 	let splitter = namespacedId.indexOf(":");
 	return `./src/${datapackId}/data/${namespacedId.substring(0, splitter)}/tags/${type}/${namespacedId.substring(splitter + 1)}.json`;
+};
+let getNamespacedFolder = (datapackId, namespacedId, type) => {
+	let splitter = namespacedId.indexOf(":");
+	let splitPath = namespacedId.substring(splitter + 1).split("/");
+	splitPath.pop();
+	return `./src/${datapackId}/data/${namespacedId.substring(0, splitter)}/tags/${type}/${splitPath.join("/")}`;
 };
 let getGeneratedData = (data) => {
 	let values = [];
@@ -54,6 +76,11 @@ for await (let line of TextReader.line((await Deno.open(`./conf/${dataReadDir}_t
 for (let category in globalMap) {
 	console.debug(`Writing "${getNamespacedPath(datapackId, category, datapackType)}"...`);
 	try {
+		let supposedPath = getNamespacedFolder(datapackId, category, datapackType);
+		if (!Deno.hasFileSync(supposedPath)) {
+			await Deno.mkdir(supposedPath, {"recursive": true});
+			console.debug(`Creating new folder under "${supposedPath}"...`);
+		};
 		await Deno.writeTextFile(getNamespacedPath(datapackId, category, datapackType), getGeneratedData(globalMap[category]));
 	} catch (err) {
 		console.error(err);
